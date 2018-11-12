e9eb5878b228705d94922581b234fb631b809522
Christoph Tack <prog.send@gmail.com>
2018-11-12 21:56:26 +0100

    Reading full time info : minutes, hours, days, months, years

--- firmware/dcf77_tests/src/simpledcf.ino
+++ firmware/dcf77_tests/src/simpledcf.ino
@@ -1,12 +1,21 @@
 #include "phaseDetector.h"
 #include "secondsDecoder.h"
 #include "binner.h"
+#include <Timezone.h> // https://github.com/JChristensen/Timezone
 
 PhaseDetector pd(PB6, PC13);
 SecondsDecoder sd;
 volatile bool secondTicked = false;
 Binner minutes(21, 7, true, 0, 59, 4);
+Binner hours(29, 6, true, 0, 23, 3);
+Binner days(36, 6, false, 1, 31, 3);
+Binner months(45, 5, false, 1, 12, 2);
+Binner years(50, 8, false, 0, 99, 4);
 bool syncMark, longPulse;
+
+TimeChangeRule myDST = {"CEST", Last, Sun, Mar, 2, +120}; //Last Sunday of March, at 2AM, go to UTC+120min
+TimeChangeRule mySTD = {"CET", Last, Sun, Oct, 3, +60};   //Last Sunday of October, at 3AM, go to UTC+60min
+Timezone myTZ(myDST, mySTD);
 
 //secondsTick is called by an ISR.  It should be kept as short as possible
 void secondsTick(const bool isSyncMark, const bool isLongPulse)
@@ -33,17 +42,47 @@ void loop()
         if (sd.getSecond(second) && second == 59)
         {
             uint64_t data = 0;
-            uint8_t minute = 0;
             sd.getTimeData(data);
             Serial1.print(data, HEX);
             minutes.update(data);
-            if (minutes.getTime(minute))
-            {
-                Serial1.print("\t");
-                Serial1.print(minute);
-            }
-            Serial1.println();
-            minutes.advanceTick();
+            hours.update(data);
+            days.update(data);
+            months.update(data);
+            years.update(data);
+            getDcfTime();
+           minutes.advanceTick();
         }
     }
 }
+
+void getDcfTime()
+{
+    uint8_t minute, hour, day, month, year;
+    minutes.getTime(minute);
+    hours.getTime(hour);
+    days.getTime(day);
+    months.getTime(month);
+    years.getTime(year);
+    char stime[100];
+    sprintf(stime, "\t%d-%d-%d %d:%d", year, month, day, hour, minute);
+    Serial1.println(stime);
+}
+
+/*Chronos::DateTime::setTime(2015, 12, 21, 17, 30, 0); //DateTime(YYYY, MM, DD, HH, mm, SS)
+Chronos::DateTime inTwoWeeks = nowTime + Chronos::Span::Weeks(2);
+PRINT(curDateTime.year());
+  PRINT('-');
+  PRINT(curDateTime.month());
+  PRINT('-');
+  PRINT(curDateTime.day());
+  PRINT(' ');
+  PRINT(curDateTime.hour());
+  PRINT(':');
+  PRINT(curDateTime.minute());
+  PRINT(':');
+  PRINT(curDateTime.second());
+  PRINTLN('!');
+      Chronos::DateTime utc(rtc.getEpoch());
+    Chronos::DateTime localTime = myTZ.toLocal(utc.asEpoch());
+
+  */
