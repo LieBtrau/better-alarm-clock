7e40a39d468b6ee26efef77c6369c8eaf0712e2d
Christoph Tack <prog.send@gmail.com>
2018-11-15 22:31:08 +0100

    Timezone now decoded

---
+++ firmware/dcf77_tests/lib/RobustDcf/timezoneDecoder.cpp
@@ -0,0 +1,39 @@
+#include "timezoneDecoder.h"
+
+TimeZoneDecoder::TimeZoneDecoder() : _bin(NR_OR_TIMEZONES){};
+
+void TimeZoneDecoder::update(SecondsDecoder::BITDATA *data)
+{
+    if (data->validBitCtr < SecondsDecoder::SECONDS_PER_MINUTE - STARTBIT)
+    {
+        //not enough valid samples in the data buffer
+        return;
+    }
+    int8_t score = 0;
+    int8_t points = _isPredictionCEST ? 1 : -1;
+    score = (data->bitShifter & CEST_BIT) ? points : -points;
+    _bin.add(0, score);
+    score += (data->bitShifter & CET_BIT) ? points : -points;
+    _bin.add(1, score);
+}
+
+void TimeZoneDecoder::setPrediction(bool isSummerTime)
+{
+    _isPredictionCEST = isSummerTime;
+}
+
+bool TimeZoneDecoder::getSecondsOffset(int16_t &offset)
+{
+    uint8_t bin = _bin.maximum(THRESHOLD);
+    if(bin==0xFF)
+    {
+        return false;
+    }
+    //At startup _isPredictionCEST is false.
+    //If we startup in winter, then bin 0 will be max and will remain max during summer time
+    //If we startup in summer, then bin 1 will be max and will remain max during winter time
+    //Sumer time (=two hour offset) happens :
+    //  when bin 1 is max with _ispredictionCEST set false or when bin 0 is max with _ispredictionCEST set true.
+    offset =  bin ^ (_isPredictionCEST ? 1 : 0) ? TWO_HOURS : ONE_HOUR;
+    return true;
+}
---
+++ firmware/dcf77_tests/lib/RobustDcf/timezoneDecoder.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Arduino.h"
+#include "secondsDecoder.h"
+#include "bin.h"
+
+class TimeZoneDecoder
+{
+  public:
+    TimeZoneDecoder();
+    void update(SecondsDecoder::BITDATA *data);
+    bool getSecondsOffset(int16_t &offset);
+    void setPrediction(bool isSummerTime);
+
+  private:
+    static const uint8_t NR_OR_TIMEZONES=2;
+    static const uint8_t STARTBIT = 17;
+    static const int8_t THRESHOLD = 1;
+    static const uint32_t CEST_BIT = 0x20000;
+    static const uint32_t CET_BIT = 0x40000;
+    static const int16_t ONE_HOUR=3600;
+    static const int16_t TWO_HOURS=7200;
+    Bin _bin;
+    bool _isPredictionCEST=false;
+};
