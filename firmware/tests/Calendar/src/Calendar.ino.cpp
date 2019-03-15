59db4c42072649bab715ee7c9cd3642d1ac687f3
Christoph Tack <prog.send@gmail.com>
2019-03-15 21:14:45 +0100

    Cleaning up calendar code

--- firmware/tests/Calendar/src/Calendar.ino.cpp
+++ firmware/tests/Calendar/src/Calendar.ino.cpp
@@ -26,38 +26,9 @@
 #include "alarmcalendar.h"
 #include "source/robustDcf.h"
 
-//Stm32RtcWrapper stmRtc;
 AlarmCalendar ac1;
-//AlarmCalendar syncCalendar;
 HardwareSerial* ser1=&Serial;
-//RobustDcf rd(PB6, PC13);
 static volatile bool alarmIsActive=false;
-
-// bool getDcfTime(Chronos::EpochTime& epoch)
-// {
-//     if(!rd.update(epoch))
-//     {
-//         return false;
-//     }
-//     ser1->println("Time updated.");
-//     if(stmRtc.setEpoch(epoch))
-//     {
-//         setTime(epoch);//Force set time, otherwise it will only be set after 300s
-//         Chronos::DateTime::now().printTo(*ser1);
-//     }
-//     return true;
-// }
-
-// time_t getRtcTime()
-// {
-//     return stmRtc.get();
-// }
-
-// void updateDcf()
-// {
-//     Chronos::EpochTime epoch;
-//     getDcfTime(epoch);
-// }
 
 void alarmCallback()
 {
@@ -69,14 +40,6 @@ void setup()
     while (!*ser1)
         ;
     ser1->begin(115200);
-    // rd.init();
-    // stmRtc.begin();
-    // setSyncProvider(getRtcTime); // the function to get the time from the RTC
-    // delay(2000);
-    // if (timeStatus() != timeSet)
-    //     ser1->println("Unable to sync with the RTC");
-    // else
-    //     ser1->println("RTC has set the system time");
     setTime(1552892378);//Monday, March 18, 2019 6:59:38 AM
 
     // printTo is a convenience method useful for debugging
@@ -90,8 +53,6 @@ void setup()
     {
         nextAlarmTime.printTo(*ser1);
     }
-    // syncCalendar.setDailyAlarm(2, 0);
-    // syncCalendar.setAlarmCallBack(updateDcf);
 }
 
 void loop()
@@ -109,14 +70,4 @@ void loop()
         ser1->println(": End of alarm");
     }
     alarmWasOn = alarmIsOn;
-
-    // if (timeStatus() != timeSet)
-    // {
-    //     Chronos::EpochTime epoch;
-    //     getDcfTime(epoch);
-    // }
-    // else
-    // {
-    //     syncCalendar.loop();
-    // }
 }
