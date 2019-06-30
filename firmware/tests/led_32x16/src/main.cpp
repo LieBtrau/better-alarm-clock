3a9855d9abd481d653f964d8423b5e38a26d9124
Christoph Tack <prog.send@gmail.com>
2019-06-30 21:39:07 +0200

    Adding some eeprom functionality

--- firmware/tests/led_32x16/src/main.cpp
+++ firmware/tests/led_32x16/src/main.cpp
@@ -5,6 +5,7 @@
 #include "actions.h"
 #include "parameters.h"
 #include "SongPlayer.h"
+#include "EEPROMAnything.h"
 
 static CommonConfig config1;
 static AlarmConfig config2;
@@ -12,6 +13,13 @@ static AlarmConfig config2;
 void setup()
 {
   Serial.begin(115200);
+  //config1.dayNight = 8;
+  //Serial.println(EEPROM_writeAnything(0, config1), DEC);
+  if(!EEPROM_readAnything(0, config1))
+  {
+    //factory default settings
+    config1.dayNight = 0;    
+  }
   assignCommonConfig(&config1);
   assignActionsConfig(&config1, &config2);
   assignAlarmConfig(&config2);
@@ -21,6 +29,7 @@ void setup()
     Serial.println("Can't init peripherals");
     while(true);
   }
+  EEPROM_writeAnything(0, config1);
 }
 
 void loop()
