b0d08cb3e075d9c1c3f9a096ca0861315cc3533c
Christoph Tack <prog.send@gmail.com>
2020-02-09 16:09:35 +0100

    rotary encoder using interrupt inputs

--- firmware/tests/encoder_test/src/main.cpp
+++ firmware/tests/encoder_test/src/main.cpp
@@ -1,5 +1,5 @@
 // -----
-// SimplePollRotator.ino - Example for the RotaryEncoder library.
+// InterruptRotator.ino - Example for the RotaryEncoder library.
 // This class is implemented for use with the Arduino environment.
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
@@ -20,26 +20,41 @@
 // Setup a RoraryEncoder for pins A2 and A3:
 RotaryEncoder encoder(A2, A3);
 
+void blink()
+{
+  encoder.tick(); // just call tick() to check the state.
+}
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("SimplePollRotator example for the RotaryEncoder library.");
-} // setup()
 
+  pinMode(A2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(A2), blink, CHANGE);
+  pinMode(A3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(A3), blink, CHANGE);
+} // setup()
 
 // Read the current position of the encoder and print out when changed.
 void loop()
 {
   static int pos = 0;
-  encoder.tick();
 
   int newPos = encoder.getPosition();
-  if (pos != newPos) {
+  if (pos != newPos)
+  {
     Serial.print(newPos);
     Serial.println();
     pos = newPos;
+
+    // Just to show, that long lasting procedures don't break the rotary encoder:
+    // When newPos is 66 the ouput will freeze, but the turned positions will be recognized even when not polled.
+    // The interrupt still works.
+    // The output is correct 6.6 seconds later.
+    if (newPos == 66)
+      delay(6600);
   } // if
 } // loop ()
 
 // The End
-
