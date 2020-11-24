d3169a283abc65f292653443a9009144734392fa
Christoph Tack <prog.send@gmail.com>
2020-11-24 21:00:27 +0100

    testing DCF-clock

--- firmware/tests/DcfDailySync/src/main.cpp
+++ firmware/tests/DcfDailySync/src/main.cpp
@@ -2,37 +2,44 @@
  *  Update UTC time once a day at 2AM.
  */
 
-#include "tm1637clock.h"
 #include "dcfUtcClock.h"
-HardwareSerial* ser1=&Serial;
+HardwareSerial *ser1 = &Serial;
 
-tm1637clock dispClock(PB12, PB13);
 DcfUtcClock dcfclock(PA8, true);
+const byte pinDisableDCF = PB1;
 // Central European Time (Frankfurt, Paris)
-TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time
-TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       // Central European Standard Time
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120}; // Central European Summer Time
+TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};   // Central European Standard Time
 Timezone CE(CEST, CET);
-
+byte lastMinute = 0;
 
 void setup()
 {
     while (!*ser1)
         ;
     ser1->begin(115200);
+    ser1->printf("Build %s\r\n", __TIMESTAMP__);
+    pinMode(pinDisableDCF, OUTPUT);
+    digitalWrite(pinDisableDCF, LOW);
     dcfclock.init();
-    dispClock.init();
 }
 
 void loop()
 {
-    if(dcfclock.update())
+    if (dcfclock.update())
     {
         time_t utc = now();
-        TimeChangeRule *tcr;        // pointer to the time change rule, use to get the TZ abbrev
+        TimeChangeRule *tcr; // pointer to the time change rule, use to get the TZ abbrev
         time_t t = CE.toLocal(utc, &tcr);
-        if(dispClock.getMinutes()!=minute(t))
+        if (lastMinute != minute(t))
         {
-            dispClock.showTime(hour(t),  minute(t));
+            lastMinute = minute(t);
+            ser1->printf("%02d:%02d\r\n", hour(t), lastMinute);
         }
+    }
+    else
+    {
+        ser1->print(".");
+        delay(1000);
     }
 }
