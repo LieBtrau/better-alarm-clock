6b95ddf3ff879a200e3842f09730719f7dcd9a35
Christoph Tack <prog.send@gmail.com>
2021-10-01 17:30:11 +0200

    Removing old sources
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- firmware/platformio/include/TimeSync.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void initClockSource();
-bool getLocalTimeSeconds(time_t& localTime);
-bool isNewMinuteStarted(time_t t, byte &hours, byte &minutes);
-bool isStillSynced();
--- firmware/platformio/include/WeekdayButtons.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Arduino.h"
-#include "inputs.h"
-
-void setWeekdayPointer(WEEKDAYS* pwd);
-bool processWeekdayButtonEvent(KEY_CODE key);
-void showWeek();
